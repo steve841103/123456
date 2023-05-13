8247c4fa9e30b8c8f35c5400ca2f1edbd06821ba
steve <steve@gmail.com>
2023-05-13 15:24:18 +0800

    修改完成2

--- demo/230513.py
+++ demo/230513.py
@@ -8,10 +8,10 @@ while i < 6:
     if x not in number:
         number.append(x)
         i += 1
-sorted(number)
-print(number, end = " ")
+print(sorted(number), end = " ")
 
 
 y= random.randint(1,49)
 print(f'特別號為{y}')
 
+#完成功能
